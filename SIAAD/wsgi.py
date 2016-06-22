ae3851287b38103ae4c7c41711696aff17688616
Mike <miguel.8a.hdez@gmail.com>
2016-06-22 17:25:09 -0500

    test heroku

--- SIAAD/wsgi.py
+++ SIAAD/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SIAAD.settings")
 

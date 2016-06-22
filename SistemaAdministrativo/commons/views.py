8d7e51754b869b6fb91a075fa63bfe81ff521999
Mike <miguel.8a.hdez@gmail.com>
2016-06-22 13:12:19 -0500

    new name SIAAD

--- SistemaAdministrativo/commons/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-
-def Error403(request, origen):
-	return render(request, 'PermisoDenegado.html', {
-			'origen': origen 
-		})
-
-def Licencias(request):
-	return render(request, 'creditos.html')
